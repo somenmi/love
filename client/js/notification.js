5d489f73ffb2e49b7a87c9559550ced758916985
somenmi <frankemos@mail.ru>
2025-04-05 02:49:30 +0300

    Changes to be committed: 	modified:   README.md 	modified:   client/css/styles.css 	modified:   client/faq.html 	modified:   client/index.html 	modified:   client/js/notification.js 	modified:   client/music.html

--- client/js/notification.js
+++ client/js/notification.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth > 768 || localStorage.getItem('notificationShown')) return;
-    
+
     const notification = document.getElementById('top-notification');
-    
+
     setTimeout(() => {
-      notification.classList.add('show');
-      
-      setTimeout(() => {
-        notification.classList.remove('show');
-        localStorage.setItem('notificationShown', 'true');
-      }, 5000);
+        notification.classList.add('show');
+
+        setTimeout(() => {
+            notification.classList.remove('show');
+            localStorage.setItem('notificationShown', 'true');
+        }, 5000);
     }, 1000);
-  });
+});
